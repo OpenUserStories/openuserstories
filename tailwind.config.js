b11f03fc7533371fea70a2cd3742577f67ba5d5a
Aaron Brethorst <aaron@brethorsting.com>
2023-10-08 15:08:06 -0700

    Create Bridgetown site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{erb,rb,html,md,js,jsx}",
+    "./frontend/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
