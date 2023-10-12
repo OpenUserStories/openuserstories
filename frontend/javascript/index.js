96d731df4817694a6f77481209d74bb6c010ee59
Aaron Brethorst <aaron@brethorsting.com>
2023-10-11 23:14:07 -0700

    Get the hamburger menu working with Alpine.js

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,3 +4,9 @@ import "$styles/index.css"
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
 import "@github/relative-time-element";
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
